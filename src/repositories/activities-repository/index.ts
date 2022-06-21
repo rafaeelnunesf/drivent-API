0bb08ced7c164e520a8aeb7a5268aadce7cdbe8f
Moises Junior <m.junior@eq.ufrj.br>
2022-06-20 23:31:52 -0300

    feat: count activity seats

--- src/repositories/activities-repository/index.ts
+++ src/repositories/activities-repository/index.ts
@@ -1,5 +1,18 @@
 import { prisma } from '@/config';
 
 export async function getActivities(dateId: number) {
-  return prisma.activities.findMany({ where: { eventDayId: dateId } });
+  return prisma.activities.findMany({
+    where: { eventDayId: dateId },
+    select: {
+      id: true,
+      local: true,
+      startTime: true,
+      endTime: true,
+      name: true,
+      seats: true,
+      _count: {
+        select: { ActivitySeats: true },
+      },
+    },
+  });
 }
